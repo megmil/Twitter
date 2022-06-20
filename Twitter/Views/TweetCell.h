59a52b06701498ecb04e94ba6b16aa91fc7f925a
megmil <megmil@umich.edu>
2022-06-20 15:59:41 -0700

    Implement a Home Timeline

---
+++ Twitter/Views/TweetCell.h
@@ -0,0 +1,29 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Megan Miller on 6/20/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetLabel;
+@property (weak, nonatomic) IBOutlet UIButton *replyButton;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIButton *messageButton;
+
+@property (strong, nonatomic) NSMutableArray *tweet; // TODO: type
+
+@end
+
+NS_ASSUME_NONNULL_END
